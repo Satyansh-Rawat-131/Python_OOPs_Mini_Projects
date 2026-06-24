67ab06dabda74e1b7a290bc5df67e7fc1d7dd57b
Satyansh-Rawat-131 <your_131_account_email@example.com>
2026-06-24 19:25:00 +0530

    created obj of the book clas for demo

--- project.py
+++ project.py
@@ -8,3 +8,11 @@ class Book:
 
     def __str__(self):
         return f"title : {self.title} \nauthor : {self.author} \nisbn : {self.isbn} \nborrowed : {"yes" if self.is_borrowed else "no"}"
+
+
+book1 = Book("Game Of Thrones" , "George R.R. Martin" , "123456789" )
+book2 = Book("Deep Learning" , "Ian Goodfellow, Yoshua Bengio, and Aaron Courville" , "987654321" )
+book3 = Book("Designing Machine Learning Systems" , "Chip Huyen" , "135792468" )
+book4 = Book("Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow" , "Aurélien Géron." , "246813579" )
+
+print(book1)
